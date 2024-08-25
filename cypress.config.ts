1ec89819d5e85f666c7dd2e3886ad97ac8d778e2
debaddy <debaddy@gmail.com>
2024-08-25 21:10:32 +0530

    OK

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:4200',
     setupNodeEvents(on, config) {
-      baseUrl: 'http://localhost:4200'
+      // implement node event listeners here
     },
+    // other configuration options
   },
 
   component: {
