ceeb046aeec1038b9449d253e0d1da6abb67a511
debaddy <debaddy@gmail.com>
2024-08-25 11:35:46 +0530

    cypress first go

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,29 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
   },
-})
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+});
 module.exports = {
   projectId: "gaccz1",
   // ...rest of the Cypress project config
-}
+};
